ec1619a6a893450cc3ed4caa05642c4540b9d6ce
Your Name <zohaibabbas8557@gmail.com>
2024-11-24 11:45:36 +0500

    commit

--- script.js
+++ script.js
@@ -214,7 +214,6 @@
 
 
       // forEach()
-
       // let newArray = [1,2,3,4,5,6,7,8,9];
       // let returnArray = newArray.forEach((item) =>{
       //   if(item % 2 == 0){
@@ -222,6 +221,7 @@
       //   }
       // });
       // console.log(returnArray);
+
 
       // filter()
 
@@ -235,12 +235,12 @@
     //sort()
     //  let sampleArray = [2,36,7,8,9,4,5,6,33];
     //  let newArray = sampleArray.sort((num1, num2)=>{
-    //   return num1<num2 ? -1 : 1;
+    //   return num1 < num2 ? -1 : 1;
     //  }); 
     // console.log(newArray);
 
     // shift() 
-    // let array = [12,3,4,5,6,7,8,9];
+    // let array = [1,2,3,4,5,6,7,8,9];
     // array.shift();
     // console.log(array);
     
@@ -260,54 +260,52 @@
     // console.log(popArray);
     
     // reverse()
-    let personNames =["John", "peterson", "halen", "johanathn"];
-    let reversedNames = personNames.reverse();
-    console.log(reversedNames);
+    // let personNames =["John", "peterson", "halen", "johanathn"];
+    // let reversedNames = personNames.reverse();
+    // console.log(reversedNames);
 
     // slice()
     let theArray = [1,2,3,4,5,6,7]
-    let slicedArray = theArray.slice(1,4)
-    console.log(slicedArray);
+    // let slicedArray = theArray.slice(1,4)
+    // console.log(slicedArray);
 
     // fill()
-    let filledArray = new Array(5).fill("dump");
-    console.log(filledArray);
+    // let filledArray = new Array(5).fill("dump");
+    // console.log(filledArray);
 
     // indexof()
-    let index = theArray.indexOf(5);
-    console.log(index);
+    // let index = theArray.indexOf(5);
+    // console.log(index);
 
     // findIndex()
-    let findIndex = theArray.findIndex(num => num > 5);
-    console.log(findIndex);
+    // let findIndex = theArray.findIndex(num => num > 5);
+    // console.log(findIndex);
 
     // includes()
-    let includes = theArray.includes(5);
-    console.log(includes);
+    // let includes = theArray.includes(5);
+    // console.log(includes);
 
     // every()
-    let every = theArray.every(num => num > 0);
-    console.log(every);
+    // let every = theArray.every(num => num > 0);
+    // console.log(every);
 
     // some()
-    let some = theArray.some(num => num > 5);
-    console.log(some);
+    // let some = theArray.some(num => num > 5);
+    // console.log(some);
 
     // reduce()
-    let reduce = theArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    console.log(reduce);
+    // let reduce = theArray.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    // console.log(reduce);
 
     // join()
-    let joinedArray = theArray.join("-");
-    console.log(joinedArray);
+    // let joinedArray = theArray.join("-");
+    // console.log(joinedArray);
     
     // concat()
-    let array1 = [1,2,3];
-    let array2 = [4,5,6];
-    let concatArray = array1.concat(array2);
-    console.log(concatArray);
+    // let array1 = [1,2,3];
+    // let array2 = [4,5,6];
+    // let concatArray = array1.concat(array2);
+    // console.log(concatArray);
    
-    // forEach()
-    array1.forEach(num => console.log(num));
 
     
