cc7d1ba45895954adf4a6c556432423c2940c820
Your Name <zohaibabbas8557@gmail.com>
2024-11-29 16:38:32 +0500

    upadted the ES6 topics

--- script.js
+++ script.js
@@ -138,7 +138,6 @@
 
 
 // ENHANCED OBJECT LITERLAS
-
 // var name = "ali"
 // var age = 34;
 // var salary = 67000;
@@ -150,7 +149,6 @@
 // console.log(emp);
 
 // you can use both varibales and objects both as once
-
 // let name2 = "ahmed"
 //    let age2 = 45
 //    let salary2 = 34000
@@ -162,7 +160,6 @@
 //    console.log(emp2);
    
 //you can also add the function name as object key
-
 // let name3 = "khan"
 // let age3 = 78
 // let emp3 ={
@@ -175,12 +172,93 @@
 // console.log(emp3);
 // emp3.showName();
 
-// ITERAOTRS AND FOR...OF
 
+// ITERAOTRS AND FOR...OF ()
+//  let array = [1,3,5,7,9,11,13,17];
+// //  for( of )
+// //  for (let x of array){
+// //   console.log(x);
+// //  }
+//  let iter = array[Symbol.iterator]();
+//  console.log(iter.next()); // 1 
+//  (iter.next()); // 2 if we skip any next() methos that element of the array will be skiped..
+//  console.log(iter.next()); // 3
+//  console.log(iter.next()); // 4   form 1 to 8 it will print all the element included in the array as it is iteratng the array...  it will always provide the output as   {value: value , done:false}
+//  console.log(iter.next()); // 5
+// if we put any message in between these iteration loop it wouldn't effect the loop
+// console.log("Hello! I am in between the iterators ");
+// console.log("Hello! I am in between the iterators ");
+// console.log("Hello! I am in between the iterators ");
+// console.log("Hello! I am in between the iterators ");
+// console.log("Hello! I am in between the iterators ");
+// console.log("Hello! I am in between the iterators ");
+//  console.log(iter.next()); // 6
+//  console.log(iter.next()); // 7  
+//  we can also print the value directly usind the ".value"  
+//  console.log(iter.next().value); // 8
+//  console.log(iter.next().done);  // in this last console the value output will be {value: undefined , done: true}  bcz the array has reached to its max length
+
+// manaully using our own loop
+
+// let str = "Muhammad"  // can also apply this method on our string characters
+// let newArray = ["ali", "khan", "ahmed"]
+// let iterArray =  newArray[Symbol.iterator]();
+//  let result = iterArray.next();
+//  while(!result.done){
+//   console.log(result.value);
+//   result = iterArray.next();
+//  }
+ 
+
+// Generators :- A generator function uses the yield keyword to generate values, pausing execution and sending values to the caller. It retains the state to resume execution after yield, continuing immediately after the last yield run.
+
+// Syntax
+// function* gen(){
+//      yield 1;
+//      yield 2;
+//      ...
+//      ...
+// }
+// These fucnvtion work same as the iterators...
+ function *myFunction(){
+// setting the value using the arguments 
+  // let result = yield;
+  // console.log(result);
+
+  // setting the multiple values using the yield array
+  // let yieldArray = [yield,yield,yield]
+  // console.log(yieldArray);
+  
+  // yield 'yield no 1';
+  // yield 'yield no 2';
+  // yield 'yield no 3';
+
+// if we have unlimmited yields
+  // let newNum = 300;
+  // while(true){
+  //   yield(newNum++)
+  // }
+ }
+    // let  f = myFunction();
+    // console.log(f.next());
+    // console.log(f.next());
+    // console.log(f.next());
+    // console.log(f.next());
+
+    // setting the yield value using the paramerters
+    // f.next();
+    // f.next(600)
+    // f.next("ali")
+    // f.next(7.87)
+
+//  use of for of () loop in generators
+// let f = myFunction()
+// for( let x of f){
+//   console.log(x)
+// }
 
 
 //ARRAY METHODS
-
 // Map()
 // let array = ['ali' , 'khan' ,'zohaib']
 // let myMap = array.map((i) =>{
@@ -190,7 +268,6 @@
 //   return i;
 // });
 // console.log(myMap);
-
 
       // let objectArray = [
       // {
@@ -265,7 +342,7 @@
     // console.log(reversedNames);
 
     // slice()
-    let theArray = [1,2,3,4,5,6,7]
+    // let theArray = [1,2,3,4,5,6,7]
     // let slicedArray = theArray.slice(1,4)
     // console.log(slicedArray);
 
@@ -307,5 +384,18 @@
     // let concatArray = array1.concat(array2);
     // console.log(concatArray);
    
+
+//PROMISES :- Promisses are mainly used for API's Calling    e.g sometimes webistes going to  the laoding state...
+// let myPromise = new Promise(function(myResolve, myReject) {
+//   let x = 0;
+//   if (x % 2 == 0) {
+//     myResolve("OK");
+//   } else {
+//     myReject("Error");
+//   }
+// });
+// myPromise.then((success)=>{
+//   console.log("Success Message")
+// }),(error)=>{console.log("Error Message")};
 
     
